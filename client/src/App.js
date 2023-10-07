a1265d8f5b8c1632d864cbade88f59eb51813808
jichani <jichan7571@naver.com>
2023-10-07 14:06:48 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/ma
 import { styled } from '@mui/system';
 import './App.css';
 import Customer from './components/Customer';
+import { Component } from 'react';
 
 // MUI 컴포넌트에 스타일을 사용하는 방식
 const StyledPaper = styled(Paper)(({ theme }) => ({
@@ -9,57 +10,49 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   marginTop: theme.spacing(3),
   overflowX: "auto",
 }));
-
 const StyledTable = styled(Table)({
   minWidth: 1080,
 });
 
-const customers = [{
-  'id': 1,
-  'image': 'http://placehold.it/100',
-  'name': '임지찬',
-  'birthday': '951220',
-  'gender': '남자',
-  'jop': '대학생',
-},
-{
-  'id': 2,
-  'image': 'http://placehold.it/100',
-  'name': '박준현',
-  'birthday': '951225',
-  'gender': '남자',
-  'jop': '대학생',
-},
-{
-  'id': 3,
-  'image': 'http://placehold.it/100',
-  'name': '장범준',
-  'birthday': '200000',
-  'gender': '남자',
-  'jop': '대학생',
-},
-];
+class App extends Component {
 
-function App() {
-  return (
-    <StyledPaper>
-      <StyledTable>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map(customer => { return (<Customer key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} jop={customer.jop} />) })}
-        </TableBody>
-      </StyledTable>
-    </StyledPaper>
-  );
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+    return (
+      <StyledPaper>
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(customer => { return (<Customer key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} jop={customer.jop} />) }) : ""}
+          </TableBody>
+        </StyledTable>
+      </StyledPaper>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get('/api/customers', (req, res) => {
+  res.send([{
+    'id': 1,
+    'image': 'http://placehold.it/100',
+    'name': '임지찬',
+    'birthday': '951220',
+    'gender': '남자',
+    'jop': '대학생',
+  },
+  {
+    'id': 2,
+    'image': 'http://placehold.it/100',
+    'name': '박준현',
+    'birthday': '951225',
+    'gender': '남자',
+    'jop': '대학생',
+  },
+  {
+    'id': 3,
+    'image': 'http://placehold.it/100',
+    'name': '장범준',
+    'birthday': '200000',
+    'gender': '남자',
+    'jop': '대학생',
+  },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port} :)`));
