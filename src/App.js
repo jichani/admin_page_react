1ad04da29be565a372e25c462ba86da4f64c56f9
jichani <jichan7571@naver.com>
2023-10-07 10:37:40 +0900

    고객 컴포넌트(Component) 만들기 - props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'name': '임지찬',
+  'birthday': '951220',
+  'gender': '남자',
+  'jop': '대학생',
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src="" alt="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      jop={customer.jop}
+    />
   );
 }
 
