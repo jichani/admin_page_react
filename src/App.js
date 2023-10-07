74607f58b51db99a79910dea8f196b35d7b263d5
jichani <jichan7571@naver.com>
2023-10-07 11:43:27 +0900

    Customer.js 생성. 고객 컴포넌트 구조화 / Map 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+const customers = [{
+  'id': 1,
+  'image': 'http://placehold.it/100',
   'name': '임지찬',
   'birthday': '951220',
   'gender': '남자',
   'jop': '대학생',
-}
+},
+{
+  'id': 2,
+  'image': 'http://placehold.it/100',
+  'name': '박준현',
+  'birthday': '951225',
+  'gender': '남자',
+  'jop': '대학생',
+},
+{
+  'id': 3,
+  'image': 'http://placehold.it/100',
+  'name': '장범준',
+  'birthday': '200000',
+  'gender': '남자',
+  'jop': '대학생',
+},
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      jop={customer.jop}
-    />
+    <div>
+      {
+        customers.map(customer => {
+          return (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              jop={customer.jop}
+            />
+          )
+        })
+      }
+    </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,12 +4,34 @@ class Customer extends React.Component {
   render() {
     return (
       <div>
+        <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+        <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} jop={this.props.jop} />
+      </div>
+    );
+  }
+}
+
+class CustomerProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.image} alt='profile' />
+        <h2>{this.props.name}({this.props.id})</h2>
+      </div>
+    )
+  }
+}
+
+class CustomerInfo extends React.Component {
+  render() {
+    return (
+      <div>
         <h2>{this.props.name}</h2>
         <p>{this.props.birthday}</p>
         <p>{this.props.gender}</p>
         <p>{this.props.jop}</p>
       </div>
-    );
+    )
   }
 }
 
