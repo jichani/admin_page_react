f4f84e8746bef3660d6af429b368f2ce1afa406d
jichani <jichan7571@naver.com>
2023-10-09 19:45:14 +0900

    15강 - 고객(Customer) 정보 삭제 기능 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -72,10 +72,11 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {this.state.customers ? this.state.customers.map(customer => { return (<Customer key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} jop={customer.job} />) }) :
+              {this.state.customers ? this.state.customers.map(customer => { return (<Customer stateRefresh={this.stateRefresh} key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} jop={customer.job} />) }) :
                 <TableRow>
                   <TableCell colSpan={6} align='center'>
                     <StyledProgress variant='determinate' value={this.state.completed} />
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,5 +1,6 @@
 import { TableCell, TableRow } from '@mui/material';
 import React from 'react';
+import CustomerDelete from './CustomerDelete';
 
 class Customer extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Customer extends React.Component {
         <TableCell>{this.props.birthday}</TableCell>
         <TableCell>{this.props.gender}</TableCell>
         <TableCell>{this.props.jop}</TableCell>
+        <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id} /></TableCell>
       </TableRow>
     );
   }
